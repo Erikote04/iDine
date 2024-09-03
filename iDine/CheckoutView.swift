824eb4f562a199616756a2e5ebc8f81e8e2b1826
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-09-04 00:34:21 +0200

    Using forms and alerts

--- iDine/CheckoutView.swift
+++ iDine/CheckoutView.swift
@@ -3,20 +3,61 @@ import SwiftUI
 struct CheckoutView: View {
     let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
     @State private var paymentType = "Cash"
+    
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    
+    let tipAmounts = [0, 10, 15, 20, 25]
+    @State private var tipAmount = 15
+    
+    @State private var isShowingAlert = false
+    
     @EnvironmentObject var order: Order
     
+    var totalPrice: String {
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(tipAmount)
+        return (total + tipValue).formatted(.currency(code: "USD"))
+    }
+    
     var body: some View {
-        VStack {
+        Form {
             Section {
                 Picker("How do you want to pay?", selection: $paymentType) {
                     ForEach(paymentTypes, id: \.self) {
                         Text($0)
                     }
                 }
+                
+                Toggle("Add iDine loyalty card", isOn: $addLoyaltyDetails.animation())
+                
+                if addLoyaltyDetails {
+                    TextField("Enter your iDine ID", text: $loyaltyNumber)
+                }
+            }
+            
+            Section("Add a tip?") {
+                Picker("Percentage", selection: $tipAmount) {
+                    ForEach(tipAmounts, id: \.self) {
+                        Text("\($0)%")
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            
+            Section("Total: \(totalPrice)") {
+                Button("Confirm Order") {
+                    isShowingAlert = true
+                }
             }
         }
         .navigationTitle("Payment")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Order Confirmed", isPresented: $isShowingAlert) {
+            // Custom alert buttons
+        } message: {
+            Text("Your total was \(totalPrice)")
+        }
     }
 }
 
